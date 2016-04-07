9c010f7fc85ef9d67136b05783cf9bfd7ce1155a
John Kokkinidis <sudoplz@gmail.com>
2016-04-07 20:21:47 +0300

    - Implemented the signup function. Now we can sign a user up. - Made code more modular. - Changed the sdk structure, much better code readability now. - Removed the abstract Backend class. - Updated Readme.md to add a bit documentation. - Updated version.

--- config.js
+++ config.js
@@ -2,19 +2,14 @@ module.exports = {
   PAV_BACKEND: {
     API_BASE_URL: 'placeavote.com',
     PRE_BASE_URL:{
-      USER_API:'https://userapi.'
+      USER_API:'https://userapidev.'  //change that to userapi when in production
     },
     ENDPOINTS: {
       USER:{
-        AUTHENTICATE_EMAIL: '/user/authenticate'
+        AUTHENTICATE_EMAIL: '/user/authenticate',
+        SIGNUP: '/user'
       }
     },
     SESSION_TOKEN_STORAGE_KEY:"token"
-  },
-  backend: {
-    pav: true,
-    parse: false,
-    hapiRemote: false,
-    hapiLocal: false
   }
 }
