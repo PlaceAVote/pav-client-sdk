9beba4473a5cdc2c9d8b1e4c13fa4b40b85bb554
John Kokkinidis <sudoplz@gmail.com>
2016-04-08 14:39:11 +0300

    - Changed the API url, fixed the functions that used the old one.

--- config.js
+++ config.js
@@ -1,15 +1,11 @@
 module.exports = {
   PAV_BACKEND: {
-    API_BASE_URL: 'placeavote.com',
-    PRE_BASE_URL:{
-      USER_API:'https://userapidev.'  //change that to userapi when in production
-    },
+    API_BASE_URL: 'https://apidev.placeavote.com',
     ENDPOINTS: {
       USER:{
         AUTHENTICATE_EMAIL: '/user/authenticate',
         SIGNUP: '/user'
       }
-    },
-    SESSION_TOKEN_STORAGE_KEY:"token"
+    }
   }
 }
