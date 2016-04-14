962a7f3e74ddaf0cd596ea6b0d25912aa2a61e69
John Kokkinidis <sudoplz@gmail.com>
2016-04-14 18:28:13 +0300

    Added back the dev BASE url.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PAV_BACKEND: {
-    API_BASE_URL: 'https://api.placeavote.com',	//or apidev
+    API_BASE_URL: 'https://apidev.placeavote.com',	//or api for prod
     ENDPOINTS: {
       USER:{
         AUTHENTICATE_EMAIL: '/user/authenticate',
