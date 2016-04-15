aef7f9dc7a14abe0489385b9a25e9a1e55a1d5c8
John Kokkinidis <sudoplz@gmail.com>
2016-04-15 16:39:48 +0300

    - Added the ability to log in using facebook credentials.

--- config.js
+++ config.js
@@ -3,7 +3,8 @@ module.exports = {
     API_BASE_URL: 'https://apidev.placeavote.com',	//or api for prod
     ENDPOINTS: {
       USER:{
-        AUTHENTICATE_EMAIL: '/user/authenticate',
+        LOGIN_EMAIL: '/user/authenticate',
+        LOGIN_FACEBOOK: '/user/facebook/authenticate',
         SIGNUP: '/user',
         SIGNUP_FACEBOOK: '/user/facebook',
         FORGOT_PASSWORD: '/password/reset'
